3b3a94f9bfe3029677fab5b267d5976b539499db
David Cooke <me@dave.lc>
2021-04-09 17:17:05 +0100

    Initial commit.

---
+++ src/main/java/uk/co/ractf/yakrazor/YakrazorApplication.java
@@ -0,0 +1,13 @@
+package uk.co.ractf.yakrazor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YakrazorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YakrazorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/uk/co/ractf/yakrazor/YakrazorApplicationTests.java
@@ -0,0 +1,13 @@
+package uk.co.ractf.yakrazor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YakrazorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
